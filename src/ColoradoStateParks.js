aa463bccb0ec7d9a5589fdc2dc051ab4324e25c6
moreenisaac <nkirotemoreen70@gmail.com>
2022-10-05 08:56:53 +0300

    done

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,13 @@
 import React from "react";
 import howManyParks from "./parks/howManyParks";
+import {elevation} from "./parks/RockyMountain";
 
 function ColoradoStateParks() {
   howManyParks(); // => "42 parks!"
 
-  return <h1>Colorado State Parks!</h1>;
+  return (<h1>Colorado State Parks!</h1>,
+  console.log(elevation)
+  )
+  
 }
+export default ColoradoStateParks;
